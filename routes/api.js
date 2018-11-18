582378b1f9ee3bd48e759b05524488d3b25c1081
Glitch (curious-nest) <none>
2018-11-18 16:15:15 +0000

    🍎💴 Checkpoint ./routes/api.js:275522/148

--- routes/api.js
+++ routes/api.js
@@ -12,6 +12,9 @@ var expect = require('chai').expect;
 
 module.exports = function (app) {
   
+  // _id, text, created_on(date&time), bumped_on(date&time, starts same as created_on), reported(boolean), delete_password, & replies(array).
+  
+  
   app.route('/api/threads/:board');
     
   app.route('/api/replies/:board');
