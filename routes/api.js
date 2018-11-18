53129b63ffa1b5c8055dc3a60ea60678dc7fb62f
Glitch (curious-nest) <none>
2018-11-18 16:48:11 +0000

    🙉🏜 Checkpoint ./routes/api.js:275522/2486

--- routes/api.js
+++ routes/api.js
@@ -23,33 +23,44 @@ module.exports = function (app) {
         text,
         delete_password,
         board,
+      } = req.params
+      const created_on = new Date()
+      const collection = db.collection('threads')
+      const data = {
+        created_on,
+        bumped_on: created_on,
+        reported: false,
+        replies: [],
+        text,
+        delete_password,
+        board,
+      }
+      await collection.insertOne(data)
+      res.redirect(`/b/${board}`)
+    })
+    .get((req, res) => {
+      req.send('')
+    })
+    
+  app.route('/api/replies/:board')
+   .post(async (req, res) => {
+      const {
+        text,
+        delete_password,
+        board,
         thread_id
       } = req.params
       const created_on = new Date()
-      const collection = db.collection('messages')
-      if (thread_id) {
-        const data = {
-          text, 
-          created_on,
-          delete_password, 
-          reported: false,
-        }
-        collection.findOneAndUpdate({_id: ObjectID(thread_id)}, { $push: { replies: data} })
-        res.redirect(`/b/${board}/${thread_id}`)
-      } else {
-        const data = {
-          created_on,
-          bumped_on: created_on,
-          reported: false,
-          replies: [],
-          text,
-          delete_password,
-          board,
-        }
-        await collection.insertOne(data)
-        res.redirect(`/b/${board}`)
+      const data = {
+        text, 
+        created_on,
+        delete_password, 
+        reported: false,
       }
+      await db.collection('threads').findOneAndUpdate({ _id: ObjectID(thread_id) }, { bumped_on: created_on })
+      
+      await db.collection('replies').insertOne({ _id: ObjectID(thread_id) }, { $push: { replies: data} })
+      
+      res.redirect(`/b/${board}/${thread_id}`)
     })
-    
-  app.route('/api/replies/:board');
 };
