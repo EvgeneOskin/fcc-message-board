55545cf5424038901f88c9f387bd69423a4c769f
Glitch (curious-nest) <none>
2018-11-18 18:44:07 +0000

    🐙🖼 Checkpoint ./routes/api.js:275522/1714

--- routes/api.js
+++ routes/api.js
@@ -25,7 +25,6 @@ module.exports = function (app) {
         board,
       } = req.params
       const created_on = new Date()
-      const collection = db.collection('threads')
       const data = {
         created_on,
         bumped_on: created_on,
@@ -35,11 +34,27 @@ module.exports = function (app) {
         delete_password,
         board,
       }
+      const collection = db.collection('threads')
       await collection.insertOne(data)
       res.redirect(`/b/${board}`)
     })
     .get((req, res) => {
       req.send('')
+    })
+    .put(async (req, res) => {
+      const {thread_id } = req.params
+      await db.collection('threads').findOneAndUpdate({ _id: ObjectID(thread_id) }, { reported: true })
+      res.send('success')
+    })
+    .delete(async (req, res) => {
+      const { thread_id, delete_password } = req.params
+      const value = await db.collection('threads').findOneAndDelete({thread_id, delete_password})
+      if (!value) {
+        res.send('incorrect password')
+      } else {
+        res.send('success')
+      }
+      
     })
     
   app.route('/api/replies/:board')
@@ -67,5 +82,21 @@ module.exports = function (app) {
     .get(async (req, res) => {
       const { thread_id } = res.query
       
+    })
+    .put(async (req, res) => {
+      const {thread_id, reply_id, board } = req.params
+      await db.collection('replies').findOneAndUpdate({ thread_id, reply_id }, { reported: true })
+      res.send('success')
+    })
+    .delete(async (req, res) => {
+      const { thread_id, delete_password } = req.params
+      const value = await db.collection('threads').findOneAndUpdate(
+        {thread_id, delete_password}, { text: '[deleted]' }
+      )
+      if (!value) {
+        res.send('incorrect password')
+      } else {
+        res.send('success')
+      }
     })
 };
